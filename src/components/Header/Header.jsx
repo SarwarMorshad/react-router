cb2d4da95c7c37fcda3e619f2b94ed29457018eb
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-25 16:18:57 +0200

    Active Route, loading Spinner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
     <div>
       <h2>Nav Bar</h2>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/users">Users</NavLink>
         <Link to="/posts">Posts</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <h2>Welcome to Home</h2>
       <Header></Header>
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? <p>Loading...</p> : <Outlet></Outlet>}
+
       <Footer></Footer>
     </div>
   );
